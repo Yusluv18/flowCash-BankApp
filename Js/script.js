46d0427ac33551b398eaad7b6c2d1838c5d821ec
Memunat Yusuf <memunat243@gmail.com>
2025-05-29 15:17:23 -0700

    add logout

--- Js/script.js
+++ Js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $(".his").click(function () {
         $(".history").slideToggle()
     })
+     $(".login").click(function () {
+        $(".mybankpage").show()
+         $(".accountpage").hide()
+    })
+     $(".logout").click(function () {
+        $(".mybankpage").hide()
+         $(".accountpage").show()
+    })
 })
