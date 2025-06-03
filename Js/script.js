5d0d3fda861aa5570941377b60f7c8ddfda04d26
Memunat Yusuf <memunat243@gmail.com>
2025-06-02 23:48:37 -0700

    add js for the hide and show on the icons

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".his").click(function () {
         $(".history").slideToggle()
     })
-     $("#login").click(function () {
+
+    $("#login").click(function () {
         $(".mybankpage").show()
-         $(".accountpage").hide()
+        $(".accountpage").hide()
     })
-     $(".logout").click(function () {
+
+    $(".logout").click(function () {
         $(".mybankpage").hide()
-         $(".accountpage").show()
+        $(".accountpage").show()
+    })
+
+    $(".profile").click(function () {
+        $(".profile-info").show()
+    })
+
+    $(".close-btn").click(function () {
+        $(".profile-info").hide()
+    })
+
+    $(".edit-icon").click(function () {
+        $(".done-icon").show()
+        $(".edit-icon").hide()
+    })
+
+    $(".done-icon").click(function () {
+        $(".edit-icon").show()
+        $(".done-icon").hide()
+    })
+
+    $("#showSignup").click(function () {
+        $("#signupForm").show()
+        $("#loginForm").hide()
+    })
+
+    $("#showLogin").click(function () {
+        $("#signupForm").hide()
+        $("#loginForm").show()
     })
 })
